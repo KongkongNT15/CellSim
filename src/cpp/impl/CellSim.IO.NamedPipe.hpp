2093517f339d4917313de01eee9b87bc6d64a1a7
KongkongNT15 <kongkong745837@gmail.com>
2025-12-16 20:44:44 +0900

    軽微な変更

--- src/cpp/impl/CellSim.IO.NamedPipe.hpp
+++ src/cpp/impl/CellSim.IO.NamedPipe.hpp
@@ -10,7 +10,7 @@
 #if CELLSIM_ENV_WINDOWS
     #include <Windows.h>
 #elif CELLSIM_ENV_UNIX
-    #include <unistd.h>
+    // 何もしないよ
 #else
         static_assert(false, "class CellSim::IO::NamedPipe is not supported.");
 #endif
